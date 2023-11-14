4325ff7b298e3aa2f8f1e76b994cecd5bb8b0f35
rohitupskills <130683027+rohitupskills@users.noreply.github.com>
2023-11-14 21:50:55 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //your JS code here. If required.
 function functionName(){
-	alert(arguments.callee.name);
+	// alert(arguments.callee.name);
+	return arguments.callee.name;
 }
-functionName()
+alert(functionName())
